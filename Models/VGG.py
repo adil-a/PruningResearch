098647b0bb4975be9b2b591188c8f85bfc390b88
adil-a <47084919+adil-a@users.noreply.github.com>
2021-06-03 17:38:30 +0400

    vgg model

--- Models/VGG.py
+++ Models/VGG.py
@@ -1,6 +1,7 @@
 # based on code taken from https://github.com/alecwangcq/GraSP/blob/master/models/base/vgg.py
 
 import torch
+import math
 import torch.nn as nn
 
 defaultcfg = {
@@ -30,8 +31,6 @@ class VGG(nn.Module):
         self.classifier = nn.Linear(cfg[-1], num_classes)
         if init_weights:
             self.apply(weights_init)
-        # if pretrained:
-        #     model.load_state_dict(model_zoo.load_url(model_urls['vgg11_bn']))
 
     def make_layers(self, cfg, batch_norm=False):
         layers = []
@@ -72,3 +71,23 @@ class VGG(nn.Module):
             elif isinstance(m, nn.Linear):
                 m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
+
+
+def weights_init(m):
+    # print('=> weights init')
+    if isinstance(m, nn.Conv2d):
+        nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        # nn.init.normal_(m.weight, 0, 0.1)
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif isinstance(m, nn.Linear):
+        # nn.init.xavier_normal(m.weight)
+        nn.init.normal_(m.weight, 0, 0.01)
+        nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.BatchNorm2d):
+        # Note that BN's running_var/mean are
+        # already initialized to 1 and 0 respectively.
+        if m.weight is not None:
+            m.weight.data.fill_(1.0)
+        if m.bias is not None:
+            m.bias.data.zero_()
