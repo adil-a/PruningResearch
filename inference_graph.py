0c5d3444893af01b6de72291c5b4b558bb3eb689
adil-a <47084919+adil-a@users.noreply.github.com>
2021-06-16 14:53:49 +0400

    code for creating channel expansion plot

--- inference_graph.py
+++ inference_graph.py
@@ -1,4 +1,6 @@
 import os
+import matplotlib.pyplot as plt
+from typing import List, Union
 
 import torch
 from torch.utils.data import DataLoader
@@ -16,37 +18,50 @@ defaultcfg = {
 }
 
 
+def multiplier(cfg: List[Union[int, str]], ratio: float):
+    for i in range(len(cfg)):
+        if isinstance(cfg[i], int):
+            cfg[i] = int(ratio * cfg[i])
+
+
 def main():
     BATCH_SIZE = 128
-    torch.manual_seed(0)
-    RATIOS = [1.5, 2.0, 2.5, 3.0, 3.5, 4.0]
-    current_ratio = RATIOS[1]
-    current_cfg = defaultcfg[11]
-    file_to_open = f'vgg11_{current_ratio}x_best.pt'
-    PATH = os.getcwd() + f'/Models/SavedModels/{file_to_open}'
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     test_transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762))]
     )
-
     testset = torchvision.datasets.CIFAR100(root='./data', train=False,
                                             download=True, transform=test_transform)
-
     testloader = DataLoader(testset, batch_size=BATCH_SIZE,
                             shuffle=False, num_workers=2)
 
-    for i in range(len(current_cfg)):
-        if isinstance(current_cfg[i], int):
-            current_cfg[i] = int(current_ratio * current_cfg[i])
+    torch.manual_seed(0)
+    RATIOS = [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0]
+    accuracies = []
+    for ratio in RATIOS:
+        current_cfg = defaultcfg[11].copy()
+        multiplier(current_cfg, ratio)
+        file_to_open = f'vgg11_{ratio}x_best.pt'
+        PATH = os.getcwd() + f'/Models/SavedModels/{file_to_open}'
+        net = get_network('vgg11', 'cifar100', current_cfg)
+        net.load_state_dict(torch.load(PATH))
+        net.to(device)
+        net.eval()
+        accuracies.append(round(val(net, testloader, device).item() * 100, 2))
 
-    net = get_network('vgg11', 'cifar100', current_cfg)
-    net.load_state_dict(torch.load(PATH))
-    net.to(device)
-    net.eval()
-
-    print(val(net, testloader, device).item())
+    plt.plot(RATIOS, accuracies, color='red', marker='o')
+    plt.title('VGG11 Channel Expansion on CIFAR100', fontsize=14)
+    plt.xlabel('Expansion Ratio', fontsize=14)
+    plt.ylabel('Test Accuracy', fontsize=14)
+    plt.grid(True)
+    for i, accuracy in enumerate(accuracies):
+        if i + 1 == len(accuracies):
+            plt.annotate(accuracy, (RATIOS[i], accuracies[i]), textcoords="offset points", xytext=(-13, -20))
+            break
+        plt.annotate(accuracy, (RATIOS[i], accuracies[i]), textcoords="offset points", xytext=(-13, 20))
+    plt.savefig(os.getcwd() + '/vgg11_channel_expansion.png')
 
 
 if __name__ == '__main__':
