5f6bec6eaee6354ae11c0d6d28e1c49a35bc2564
adil-a <47084919+adil-a@users.noreply.github.com>
2021-06-16 15:14:20 +0400

    fixed up channel expansion plotting code

--- inference_graph.py
+++ inference_graph.py
@@ -57,10 +57,10 @@ def main():
     plt.ylabel('Test Accuracy', fontsize=14)
     plt.grid(True)
     for i, accuracy in enumerate(accuracies):
-        if i + 1 == len(accuracies):
+        if i + 1 == len(accuracies) or i + 1 == len(accuracies) - 1:
             plt.annotate(accuracy, (RATIOS[i], accuracies[i]), textcoords="offset points", xytext=(-13, -20))
-            break
-        plt.annotate(accuracy, (RATIOS[i], accuracies[i]), textcoords="offset points", xytext=(-13, 20))
+        else:
+            plt.annotate(accuracy, (RATIOS[i], accuracies[i]), textcoords="offset points", xytext=(-13, 20))
     plt.savefig(os.getcwd() + '/vgg11_channel_expansion.png')
 
 
